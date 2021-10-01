886241e5bb33b4e80a2f65284c846a08622418a3
Ayush Tickoo <48850318+007tickooayush@users.noreply.github.com>
2021-10-01 13:43:12 +0530

    update, delete implemented

--- src/main/java/com/reazon/dao/UserDAO.java
+++ src/main/java/com/reazon/dao/UserDAO.java
@@ -10,20 +10,18 @@ import java.util.List;
 
 import com.reazon.model.User;
 
-
-
+// Database Access Object Class {UserDAO}
 public class UserDAO {
+	
 	private String url = "jdbc:mysql://localhost:3308/demo?useSSl=false";
 	private String username = "root";
 	private String password = "root";
 	
-	private static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES " +
-	        " (?, ?, ?);";
-	private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
-	
-	private static final String SELECT_ALL_USERS = "select * from users";
-	private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
-    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+	private static final String INSERT_QUERY = "INSERT_QUERY INTO users (name, email, country) VALUES (?, ?, ?);";
+	private static final String SELECT_ALL = "select * from users";
+	private static final String SELECT_BY_ID = "select id,name,email,country from users where id =?";
+	private static final String DELETE_QUERY = "delete from users where id = ?;";
+    private static final String UPDATE_QUERY = "update users set name = ?,email= ?, country =? where id = ?;";
     
     
     protected Connection getConnection() {
@@ -38,10 +36,10 @@ public class UserDAO {
     	
     	return conn;
     }
-    
+//    INSERT_QUERY [User]
     public void insertUser(User user) throws SQLException {
     	Connection conn = getConnection();
-    	PreparedStatement preparedStatement = conn.prepareStatement(INSERT_USERS_SQL);
+    	PreparedStatement preparedStatement = conn.prepareStatement(INSERT_QUERY);
     	
     	preparedStatement.setString(1, user.getName());
     	preparedStatement.setString(2, user.getEmail());
@@ -50,10 +48,11 @@ public class UserDAO {
     	preparedStatement.executeUpdate();
     }
  
+//    SELECT [User] entity set
     public List<User> selectAllUsers() throws SQLException{
     	List<User> users = new ArrayList<>();
     	Connection conn = getConnection();
-    	PreparedStatement preparedStatement = conn.prepareStatement(SELECT_ALL_USERS);
+    	PreparedStatement preparedStatement = conn.prepareStatement(SELECT_ALL);
     	
     	ResultSet rs = preparedStatement.executeQuery();
     	
@@ -62,6 +61,50 @@ public class UserDAO {
     	}
     	
     	return users;
+    }
+    
+//    SELECT [User] BY ID 
+    public User getUserByID(int id) throws SQLException {
+    	User existingUser = null;
+    	Connection conn = getConnection();
+    	PreparedStatement preparedStatement = conn.prepareStatement(SELECT_BY_ID);
+    	preparedStatement.setInt(1, id);
+    	ResultSet rs = preparedStatement.executeQuery();
+    	
+    	while(rs.next()) {
+    		existingUser = new User(rs.getInt("id"),rs.getString("name"),rs.getString("email"),rs.getString("country"));
+    	}
+    	return existingUser;
+    }
+    
+//    DELETE_QUERY [User]
+    public boolean deleteUser(int id) throws SQLException {
+    	
+    	Connection conn = getConnection();
+    	PreparedStatement preparedStatement = conn.prepareStatement(DELETE_QUERY);
+    	
+    	preparedStatement.setInt(1, id);
+    	
+    	boolean rowDeleted = preparedStatement.executeUpdate() > 0;
+    	
+    	return rowDeleted;
+    }
+    
+//    UPDATE [User]
+    public boolean updateUser(User existingUser) throws SQLException {
+    	
+    	Connection conn = getConnection();
+    	PreparedStatement preparedStatement = conn.prepareStatement(UPDATE_QUERY);
+    	
+    	preparedStatement.setInt(4, existingUser.getId());
+    	
+    	preparedStatement.setString(1, existingUser.getName());
+    	preparedStatement.setString(2, existingUser.getEmail());
+    	preparedStatement.setString(3, existingUser.getCountry());
+    	
+    	boolean rowUpdated = preparedStatement.executeUpdate() > 0;
+    	
+    	return rowUpdated;
     }
 }
 
